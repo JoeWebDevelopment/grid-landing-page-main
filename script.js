d09b1292bf78db90cabeefd9336859c3f008435b
JoeWebDevelopment <JoeWebDevelopment@protonmail.com>
2026-08-25 16:22:06 -0400

    add keyboard and overlay menu dismissal

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 let menuButton = document.querySelector('button')
 let buttonImg = document.querySelector('.menu-img')
 let primaryNav = document.querySelector('.primary-nav')
-let main = document.querySelector('main')
 let overlay = document.querySelector('.overlay')
 
-let OpenMenuOrCloseMenu = ()=>{
+let openMenuOrCloseMenu = ()=>{
     if(buttonImg.getAttribute("src") === "assets/images/icon-menu.svg"){
         primaryNav.classList.remove('hidden')
         menuButton.setAttribute('aria-expanded', 'true')
+        menuButton.setAttribute('aria-label', 'Close menu')
         buttonImg.setAttribute('src', "assets/images/icon-close.svg")
         overlay.classList.remove('hidden')
     }
@@ -17,14 +17,26 @@ let OpenMenuOrCloseMenu = ()=>{
             primaryNav.classList.add('hidden')
            buttonImg.setAttribute('src', "assets/images/icon-menu.svg")
            menuButton.setAttribute('aria-expanded', 'false')
-           overlay.classList.add('hidden') 
+           overlay.classList.add('hidden')
+           menuButton.setAttribute('aria-label', 'Open menu') 
         }
     }
 
+document.addEventListener('keydown', function(event) {     if (event.key === 'Escape') {        
+if(menuButton.getAttribute('aria-expanded') === "true"){
+openMenuOrCloseMenu()
+} else{
+return;
+}
+  } });
 
+
+overlay.addEventListener('click', () =>{
+    openMenuOrCloseMenu()
+})
 
 menuButton.addEventListener('click', ()=>{
-    OpenMenuOrCloseMenu()
+    openMenuOrCloseMenu()
 })
 
 
